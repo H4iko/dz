867b1b8a4d7ed73bd9ad1dd8815c0c70554be955
H4iko <rugby2017@bk.ru>
2022-06-04 15:01:47 +0300

    update

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main (String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        intList.sort(Comparator.naturalOrder());
+        for (Integer item : intList) {
+            if (item > 0 && item % 2 == 0) {
+                System.out.println(item);
+            }
+        }
+    }
+}
